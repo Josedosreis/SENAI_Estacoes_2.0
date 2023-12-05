cf2d4946bc43116599642f7f1abe3d14e618c5ec
Josedosreis <joseleand06@gmail.com>
2023-12-05 15:29:51 -0300

    estacoes do ano refinada atualizada

--- refinada.py
+++ refinada.py
@@ -8,33 +8,31 @@ mes = int(input('Digite o mes: '))
 
 #Processamento
 
-if dia >= 20  and dia <= 31 and mes == 1 or dia >= 1 and dia <= 20 and mes == 2:
+if dia >= 1 and dia <= 31 and mes == 2:
     print('Voce nasceu no Verão !')
-elif dia >= 20 and dia <= 29 and mes == 2 or dia >= 1 and dia <= 20 and mes == 3:
+elif dia >= 1 and dia <= 19 and mes == 3:
     print('Voce nasceu no Verão !')
-elif dia >= 21 and dia <= 31 and mes == 3 or dia >= 1 and dia <= 19 and  mes == 4:
+elif dia >= 20 and dia <= 31 and mes == 3 or dia >= 1 and dia <= 30 and  mes == 4:
     print('Você nasceu no outono')
-elif dia >= 20 and dia <= 30 and mes == 4 or dia >= 1 and dia <=19 and mes == 5:
+elif dia >= 1 and dia <= 31 and mes == 5:
     print('Você nasceu no outono')
-elif dia >= 20 and dia <= 31 and mes == 5 or dia >= 1 and dia <= 19 and mes == 6:
+elif dia >= 1 and dia <= 20 and mes == 6:
     print('Você nasceu no outono')
-elif dia >= 21 and dia <= 30 and mes == 6 or dia >= 1 and dia <= 19 and mes == 7:
+elif dia >= 21 and dia <= 30 and mes == 6 or dia >= 1 and dia <= 31 and mes == 7:
     print("Você nasceu no inverno")
-elif dia >= 20 and dia <= 31 and mes == 7 or dia >= 1 and dia <= 19 and mes == 8:
+elif dia >= 1 and dia <= 31 and mes == 8:
     print("Você nasceu no inverno")
-elif dia >= 20 and dia <= 31 and mes == 8 or dia >= 1 and dia <= 22 and mes == 9:
+elif dia >= 1 and dia <= 22 and mes == 9:
     print("Você nasceu no inverno")
-elif dia >= 23 and dia <= 30 and mes == 9 or dia >= 1 and dia <= 19 and mes == 10:
+elif dia >= 23 and dia <= 30 and mes == 9 or dia >= 1 and dia <= 31 and mes == 10:
     print("Você nasceu na primavera")
-elif dia >= 20 and dia <= 31 and mes == 10 or dia >= 1 and dia <= 19 and mes == 11:
+elif dia >= 1 and dia <= 30 and mes == 11:
     print("Você nasceu na primavera")
-elif dia >= 20 and dia <= 30 and mes == 11 or dia >= 1 and dia <= 21 and mes == 12:
+elif dia >= 1 and dia <= 21 and mes == 12:
     print("Você nasceu na primavera")
-elif dia >= 22 and dia <= 31 and mes == 12 or dia >= 1 and dia <= 20 and mes == 1:
+elif dia >= 22 and dia <= 31 and mes == 12 or dia >= 1 and dia <= 31 and mes == 1:
         print('Voce nasceu no Verão !')
 else:
     print('Invalido')
-
-
 
 
